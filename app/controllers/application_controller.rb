e6e4760564a5ac9496f582197d7cf0f77c8b5663
Garrett <garres4@vt.edu>
2021-12-15 16:11:24 -0700

    commit tree a9a1ddc19873bde30bd6aba9cc493e0a7d4e0dd4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_record_not_found_error
+  rescue_from ActiveRecord::RecordInvalid, with: :rescue_from_record_invalid_error
+
+  def rescue_from_record_invalid_error
+    render json: { error: "Spice not found." }, status: :not_found
+  end
+
+  def rescue_from_record_invalid_error(invalid)
+    render json: { error: invalid.records.errors.full_messages }, status: :unprocessable_entity
+  end
+
 
 end
